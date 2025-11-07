3e694809c2428cdd3a9b22d13ee736dc33283ca5
ggalas-cell <ggalas@gaming.tech>
2025-11-07 09:05:24 +0100

    feat : upd

--- Dungeon Dragon/Dungeon Dragon/enemys.h
+++ Dungeon Dragon/Dungeon Dragon/enemys.h
@@ -1,10 +1,17 @@
 #pragma once
 #include "character.h"
 
-class enemys : public character
-{
-private:
-	int title;
-	int giveexp;
+class enemies : public character
+{   
+public:
+	void lvup()
+	{
+		lv++;
+		maxHp *= 1.3;
+		currentHp *= 1.3;
+		if (currentHp > maxHp)currentHp = maxHp;
+		dmg *= 1.3;
+		speed *= 1.3;
+	}
+	enemies(const std::string& n, float ch, float mh, float d, float sp, float lv) : character(n, ch, mh, d, sp, lv) {}
 };
-
